346572a54adcd05d69cb8f31160c1fbd0f50d675
IanJames2 <ianjms81@gmail.com>
2021-05-28 17:37:23 -0500

    Added Data_Types_for_Leonids_Toys directory to leonids-toys.

---
+++ Data_Types_for_Leonids_Toys/leonids-toys-properties.js
@@ -0,0 +1,11 @@
+/*
+◾ Name: {type: String}
+◾ Price: {type: Number} //Float
+◾ Brand: {type: String}
+◾ Design: {type: String} 
+◾ Quality: {type: String}
+◾ Lifespan: {type: String}
+◾ Warranty: {type: Boolean}
+◾ Warranty_Description: {type: String}
+◾ Durability: {type: String}
+*/
